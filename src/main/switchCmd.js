42ff74128b80f8f13a9d318f60550884b85c424d
SPECTRA995 <maxmillerasce@gmail.com>
2022-12-25 11:33:05 +0300

    feat: add cp cr del files

--- src/main/switchCmd.js
+++ src/main/switchCmd.js
@@ -1,5 +1,9 @@
 import { cd } from "../commands/cd.js";
+import { copy } from "../commands/copy.js";
+import { create } from "../commands/create.js";
+import { del } from "../commands/delete.js";
 import { up } from "../commands/up.js";
+import { endWork } from "./console.js";
 
 export const switchCmd = async (data) => {
   //console.log('data', data);
@@ -13,6 +17,22 @@ export const switchCmd = async (data) => {
       break;
     case "cd":
       cd(path);
+      break;
+    case "cp":
+      await copy(path, newPath);
+      break;
+    case "rm":
+      await del(path);
+      break;
+    case "add":
+      await create(path);
+      break;
+    case ".exit":
+      if (path) {
+        console.log("Invalid input");
+      } else {
+        endWork();
+      }
       break;
     default:
       console.log("Invalid input");
